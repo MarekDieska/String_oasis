12ebbcb61e0f8deb853d214973ac9c250ecc6bdc
Viktória Latičová <xlaticova@stuba.sk>
2025-03-12 14:18:22 +0100

    edit

--- project/category.php
+++ project/category.php
@@ -5,4 +5,5 @@
     <li><a href="#"><img src="images/git.svg" alt="gitary" width="70"></a></li>
     <li><a href="#"><img src="images/git.svg" alt="gitary" width="70"></a></li>
     <li><a href="#"><img src="images/git.svg" alt="gitary" width="70"></a></li>
+    <li><a href="#"><img src="images/git.svg" alt="gitary" width="70"></a></li>
 </ul>
