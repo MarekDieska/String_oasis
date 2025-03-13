025152f28b7dca77c4b4c01731fff0b6d397e89c
Viktória Latičová <xlaticova@stuba.sk>
2025-03-13 16:03:48 +0100

    changes

--- project/components/main.php
+++ project/components/main.php
@@ -1,6 +1,18 @@
-<div class="grad">
+<article class="grad">
     <h1>"Struny vašich snov, melódia vášho srdca."</h1>
-</div>
-<article>
-    <img src="../images/kombo.svg" alt="Logo" width="150">
 </article>
+
+<section class="container_pre_zlavy">
+    <p class="zlava">Zľavy</p>
+    <div class="produkty_zlava">
+        <?php
+        for($i = 0; $i < 10; $i++){
+            include "../components/produkt_v_zlave.php";
+        }
+        ?>
+    </div>
+</section>
+
+<section>
+
+</section>
---
+++ project/components/produkt_v_zlave.php
@@ -0,0 +1,5 @@
+<div class="produkt">
+    <img src="../images/kombo.svg" alt="photo" width="100">
+    <div><p>Názov</p></div>
+    <div><p>Cena €</p></div>
+</div>
