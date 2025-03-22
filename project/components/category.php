f80d4c2a46146a59dd3b62a52d2b0350c6395fc3
MarekDieska <xdieska@stuba.sk>
2025-03-22 15:47:12 +0100

    zmena category gridu

--- project/components/category.php
+++ project/components/category.php
@@ -1,58 +1,56 @@
-
-<ul class="category">
-    <li class="category_group">
-        <a href="#">
+<div class = "row category">
+    <div class="col-6 col-md-2 category_group">
+        <a href="#" data-bs-toggle="collapse" data-bs-target="#gitaryCollapse">
             <img src="../images/git.svg" alt="gitary" height="70">
             <p>GITARY</p>
-            <ul class="dropdown">
-                <li>elektrické</li>
-                <li>elektro-akustické</li>
-                <li>akustické</li>
-                <li>klasické</li>
-                <li>detské</li>
-            </ul>
         </a>
-    </li>
-    <li class="category_group">
-        <a href="#">
+        <ul id="gitaryCollapse" class="collapse dropdown">
+            <li>elektrické</li>
+            <li>elektro-akustické</li>
+            <li>akustické</li>
+            <li>klasické</li>
+            <li>detské</li>
+        </ul>
+    </div>
+    <div class="col-6 col-md-2 category_group">
+        <a href="#" data-bs-toggle="collapse" data-bs-target="#basgitaryCollapse">
             <img src="../images/bass.svg" alt="gitary" height="70">
             <p>BASGITARY</p>
-            <ul class="dropdown" >
-                <li>4 strunové</li>
-                <li>5 strunové</li>
-            </ul>
         </a>
-    </li>
-    <li class="category_group">
-        <a href="#">
+        <ul id="basgitaryCollapse" class="collapse dropdown">
+            <li>4 strunové</li>
+            <li>5 strunové</li>
+        </ul>
+    </div>
+    <div class="col-6 col-md-2 category_group">
+        <a href="#" data-bs-toggle="collapse" data-bs-target="#ineStrunyCollapse">
             <img src="../images/banjo.svg" alt="gitary" height="70">
             <p>INÉ STRUNY</p>
-            <ul class="dropdown" id="">
-                <li>ukulele</li>
-                <li>banjo</li>
-                <li>mandolína</li>
-                <li>kora</li>
-            </ul>
         </a>
-    </li>
-    <li class="category_group">
+        <ul id="ineStrunyCollapse" class="collapse dropdown">
+            <li>ukulele</li>
+            <li>banjo</li>
+            <li>mandolína</li>
+            <li>kora</li>
+        </ul>
+    </div>
+    <div class="col-6 col-md-2 category_group">
         <a href="#">
             <img src="../images/kombo.svg" alt="gitary" height="70">
             <p>KOMBÁ</p>
         </a>
-    </li>
-    <li class="category_group">
+    </div>
+    <div class="col-6 col-md-2 category_group">
         <a href="#">
             <img src="../images/kabel.svg" alt="gitary" height="70">
             <p>PRÍSLUŠENSTVO</p>
         </a>
-    </li>
-    <li class="category_group">
+    </div>
+    <div class="col-6 col-md-2 category_group">
         <a href="#">
             <img src="../images/platna.svg" alt="gitary" height="70">
             <p>PLATNE</p>
         </a>
-    </li>
+    </div>
     <script src="../javaScript/category_scroll.js"></script>
-</ul>
-
+</div>
