20e49ab392a252bed6649f6fdf2ba8ba051e6e32
Viktória Latičová <xlaticova@stuba.sk>
2025-03-22 16:07:36 +0100

    header bootstrap

--- project/components/header.php
+++ project/components/header.php
@@ -1,8 +1,8 @@
 <div class="container-fluid m-0">
     <div class="row d-flex justify-content-between align-items-center w-100">
         <div class="col-md-4 d-flex align-items-center">
-            <a href="#" class="navbar-brand d-flex align-items-center m-0">
-                <img src="../images/logo.svg" alt="Logo" width="70">
+            <a href="#" class="navbar-brand d-flex align-items-center m-0 p-0">
+                <img src="../images/logo.svg" alt="Logo" width="60">
                 <p class="mb-0 ms-2 fs-3 logo-p">String Oasis</p>
             </a>
         </div>
@@ -12,18 +12,18 @@
                 <input type="text" placeholder="Hľadať podľa názvu..." class="form-control input-custom rounded-pill border-0">
             </label>
 
-            <ul class="nav_help d-flex list-unstyled mt-2 mb-0 ">
-                <li><a class="link-custom" href="#">Mapa</a></li>
-                <li><a class="link-custom" href="#">FAQ</a></li>
-                <li><a class="link-custom" href="#">Reklamácie</a></li>
+            <ul class="nav_help d-flex list-unstyled mt-2 mb-0 fs-6">
+                <li class="ms-3"><a class="link-custom" href="#">Mapa</a></li>
+                <li class="ms-3"><a class="link-custom" href="#">FAQ</a></li>
+                <li class="ms-3"><a class="link-custom" href="#">Reklamácie</a></li>
             </ul>
         </div>
 
         <div class="col-md-4 d-flex justify-content-end align-items-center">
-            <ul class="d-flex list-unstyled m-0">
-                <li class="ms-3 link-custom"><a href="#">Košík</a></li>
-                <li class="ms-3 link-custom"><a href="#">Účet</a></li>
-                <li class="ms-3 link-custom"><a href="#">&#x2661</a></li>
+            <ul class="d-flex list-unstyled fs-4 m-0">
+                <li class="ms-3"><a class="link-custom" href="#">Košík</a></li>
+                <li class="ms-3"><a class="link-custom" href="#">Účet</a></li>
+                <li class="ms-3"><a class="link-custom" href="#">&#x2661</a></li>
             </ul>
         </div>
     </div>
