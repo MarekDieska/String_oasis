22de5289d1dd44d5567dd0442344dc71ead2119f
MarekDieska <xdieska@stuba.sk>
2025-03-22 16:07:34 +0100

    zmena category gridu

--- project/pages/index.php
+++ project/pages/index.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <style>
         #categoryNav {
+            position: sticky;
             top: 0;
             background-color: white;
             padding-top: 100px;
@@ -18,22 +19,9 @@
     </style>
 </head>
 <body>
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     <header class="navbar fixed-top header-custom">
         <?php include "../components/header.php" ?>
     </header>
-=======
-<header>
-    <?php include "../components/header.php" ?>
-</header>
->>>>>>> Stashed changes
-=======
-<header>
-    <?php include "../components/header.php" ?>
-</header>
->>>>>>> Stashed changes
 
 <nav id = categoryNav> <?php include "../components/category.php" ?>
 </nav>
