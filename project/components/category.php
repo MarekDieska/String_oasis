005894eec71326e6dc7c2d63105fa53e70a1b0f7
MarekDieska <xdieska@stuba.sk>
2025-03-22 17:46:31 +0100

    skoro hotove kategorie a navbar

--- project/components/category.php
+++ project/components/category.php
@@ -1,9 +1,9 @@
-<div class = "m-0 category-custom">
+<div class = "m-0">
     <div class = "row d-flex w-100">
         <div class="col-md-2 d-flex flex-column-align-items-center justify-content-center">
             <a href="#" data-bs-toggle="collapse" data-bs-target="#gitaryCollapse">
                 <img src="../images/git.svg" alt="gitary" height="70">
-                <p>GITARY</p>
+                <p class="m-0">GITARY</p>
             </a>
             <ul id="gitaryCollapse" class="collapse dropdown">
                 <li>elektrické</li>
@@ -16,7 +16,7 @@
         <div class="col-md-2 d-flex flex-column-align-items-center justify-content-center">
             <a href="#" data-bs-toggle="collapse" data-bs-target="#basgitaryCollapse">
                 <img src="../images/bass.svg" alt="gitary" height="70">
-                <p>BASGITARY</p>
+                <p class="m-0">BASGITARY</p>
             </a>
             <ul id="basgitaryCollapse" class="collapse dropdown">
                 <li>4 strunové</li>
@@ -26,7 +26,7 @@
         <div class="col-md-2 d-flex flex-column-align-items-center justify-content-center">
             <a href="#" data-bs-toggle="collapse" data-bs-target="#ineStrunyCollapse">
                 <img src="../images/banjo.svg" alt="gitary" height="70">
-                <p>INÉ STRUNY</p>
+                <p class="m-0">INÉ STRUNY</p>
             </a>
             <ul id="ineStrunyCollapse" class="collapse dropdown">
                 <li>ukulele</li>
@@ -38,19 +38,19 @@
         <div class="col-md-2 d-flex flex-column-align-items-center justify-content-center">
             <a href="#">
                 <img src="../images/kombo.svg" alt="gitary" height="70">
-                <p>KOMBÁ</p>
+                <p class="m-0">KOMBÁ</p>
             </a>
         </div>
         <div class="col-md-2 d-flex flex-column-align-items-center justify-content-center">
             <a href="#">
                 <img src="../images/kabel.svg" alt="gitary" height="70">
-                <p>PRÍSLUŠENSTVO</p>
+                <p class="m-0">PRÍSLUŠENSTVO</p>
             </a>
         </div>
         <div class="col-md-2 d-flex flex-column-align-items-center justify-content-center">
             <a href="#">
                 <img src="../images/platna.svg" alt="gitary" height="70">
-                <p>PLATNE</p>
+                <p class="m-0">PLATNE</p>
             </a>
         </div>
         <script src="../javaScript/category_scroll.js"></script>
--- project/javaScript/category_scroll.js
+++ project/javaScript/category_scroll.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     const category = document.querySelector('.category-custom');
-    if (window.scrollY > 10) {
+    if (window.scrollY > 100) {
         category.classList.add('scrolled');
     } else {
         category.classList.remove('scrolled');
