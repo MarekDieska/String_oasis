c454b6de02b8d9045cc6e824f227ab003cca2d79
MarekDieska <xdieska@stuba.sk>
2025-03-24 19:17:56 +0100

    login form 2

--- project/pages/index.php
+++ project/pages/index.php
@@ -32,28 +32,5 @@
         </footer>
     </body>
 
-    <div class="modal fade" id="login_form" tabindex="-1" aria-labelledby="LoginForm" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content login-form-color">
-                <div class="modal-body">
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-                    <div class="form-area login-form-color">
-                        <h1 class="text-center login-form-label">Prihlásenie</h1>
-                        <form>
-                            <div class="mb-2 mt-2">
-                                <label for="exampleInputEmail1" class="form-label login-form-label">Email</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            </div>
-                            <div class="mb-2">
-                                <label for="exampleInputPassword1" class="form-label login-form-label">Heslo</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
-                            </div>
-                                <button type="submit" class="btn bg-white w-100 mt-5 mb-2">LOGIN</button>
-                                <p><a href="#" class="login-form-label link-light justify-content-center d-flex">Registrácia</a></p>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php include "../components/login.php" ?>
 </html>
