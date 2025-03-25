137729f9abdd4f59c80a446ded1ed93611e7f7e9
MarekDieska <xdieska@stuba.sk>
2025-03-25 23:28:09 +0100

    detail produktu bez recenzii

--- project/components/main_detail.php
+++ project/components/main_detail.php
@@ -3,7 +3,7 @@
         <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb text_color">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="../pages/index.php">Domov</a></li>
-                <li class="breadcrumb-item"><a href="#">Produkty</a></li>
+                <li class="breadcrumb-item"><a href="../pages/filters_page.php">Produkty</a></li>
                 <li class="breadcrumb-item"><a href="#">Detail</a></li>
             </ol>
         </nav>
@@ -13,7 +13,7 @@
         </div>
     </div>
 
-    <div class="col-md-6 text_color d-flex flex-column mt-4">
+    <div class="col-md-6 text_color d-flex flex-column mt-4 ms-lg-5">
         <p class="fs-1 pt-1">HarmonyTone Sunset D-50</p>
         <h5>HarmonyTone Sunset D-50 je perfektná pre začínajúcich aj pokročilých hudobníkov, ktorí hľadajú spoľahlivý nástroj s výborným zvukom a luxusným vzhľadom.</h5>
 
@@ -63,4 +63,58 @@
 
     <hr class="text_color mt-5">
     <h1 class="text_color detail_large_text mt-md-1">HarmonyTone Sunset D-50</h1>
+    <h5 class="text_color" style="font-family: 'Times New Roman', sans-serif;">HarmonyTone Sunset D-50
+        HarmonyTone Sunset D-50 je kvalitná elektro-akustická gitara, ktorá kombinuje klasický dizajn s moderným spracovaním. Jej telo je vyrobené z výberového mahagónu, ktorý zabezpečuje hlboký, bohatý tón s teplými basmi a jasnými výškami. Vrchná doska zo smreku pridáva rezonanciu a dynamiku, vďaka čomu je ideálna pre hráčov všetkých úrovní.
+        <br><br>
+        Výnimočný sunburst lak dodáva gitare elegantný a nadčasový vzhľad, zatiaľ čo ergonomický výrez umožňuje jednoduchý prístup k vyšším pražcom. Integrovaný predzosilňovač s ekvalizérom a zabudovanou ladičkou zaručuje, že váš zvuk bude vždy presný a vyvážený, či už hráte doma, v štúdiu alebo na pódiu.
+    </h5>
+
+    <div class="d-flex justify-content-center col-md-3">
+        <div class="p-3">
+            <div class="card">
+                <img src="../images/bg.jpg" class="card-img-top" alt="...">
+            </div>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center col-md-3">
+        <div class="p-3 ">
+            <div class="card">
+                <img src="../images/bg.jpg" class="card-img-top" alt="...">
+            </div>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center col-md-3">
+        <div class="p-3 ">
+            <div class="card">
+                <img src="../images/bg.jpg" class="card-img-top" alt="...">
+            </div>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center col-md-3">
+        <div class="p-3">
+            <div class="card">
+                <img src="../images/bg.jpg" class="card-img-top" alt="...">
+            </div>
+        </div>
+    </div>
+
+    <hr class="text_color mt-5">
+
+    <h3 class="text_color text-center mt-1" style="font-family: 'Times New Roman', sans-serif;">
+        <i>Telo:</i> <br>Mahagón so smrekovou vrchnou doskou
+        <br><br>
+        <i>Krk:</i> <br>Mahagón s palisandrovým hmatníkom
+        <br><br>
+        <i>Farba:</i> <br>Sunburst
+        <br><br>
+    <i>Počet pražcov:</i> <br>20
+        <br><br>
+    <i>Elektronika:</i> <br>Predzosilňovač s 3-pásmovým ekvalizérom a zabudovanou ladičkou
+        <br><br>
+    <i>Kobylka:</i> <br>Palisander pre optimálny prenos vibrácií
+    </h3>
+
+    <hr class="text_color mt-5">
+
+    <h1 class="mt-md-3 text_color align-items-left">Recenzie</h1>
 </div>
