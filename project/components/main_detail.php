9351fa0ee5b2783a342a7bf3cdd1309894400b3f
MarekDieska <xdieska@stuba.sk>
2025-03-25 12:47:50 +0100

    detail produktu obrazok

--- project/components/main_detail.php
+++ project/components/main_detail.php
@@ -1,18 +1,20 @@
+<div class="row">
+    <div class="col-md-6 d-flex flex-column justify-content-center product-detail mt-lg-5 mt-0 mb-3">
+        <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb text_color">
+            <ol class=breadcrumb>
+                <li class="breadcrumb-item"><a href="../pages/index.php">Domov</a></li>
+                <li class="breadcrumb-item"><a href="#">Produkty</a></li>
+                <li class="breadcrumb-item"><a href="#">Detail</a></li>
+            </ol>
+        </nav>
 
-<nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb text_color">
-    <br><br><br><br><br><br><br><br><br>
-    <ol class=breadcrumb>
-        <li class="breadcrumb-item"><a href="../pages/index.php">Domov</a></li>
-        <li class="breadcrumb-item"><a href="#">Produkty</a></li>
-        <li class="breadcrumb-item"><a href="#">Detail</a></li>
-    </ol>
-</nav>
+        <div class="card border-danger border-5 border-opacity-75">
+            <img src="../images/product_guitar.jpg" class="card-img-top" alt="...">
+        </div>
+    </div>
 
-<div class="container text_color">
-    <br><br><br><br><br><br><br><br><br>
-    <h1>toto je detail produktu.</h1>
-    <h2>dakujem za pozornost</h2>
-    <br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br>
+    <div class="col-md-6 text_color align-items-center">
+        <h1>toto je detail produktu.</h1>
+        <h2>dakujem za pozornost</h2>
+    </div>
 </div>
