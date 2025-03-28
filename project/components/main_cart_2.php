7bc1e4b845b742eec02b8c00a643b5bcdc2da846
MarekDieska <xdieska@stuba.sk>
2025-03-28 23:15:57 +0100

    zadanie udajov kosik

--- project/components/main_cart_2.php
+++ project/components/main_cart_2.php
@@ -1,27 +1,51 @@
-<div class="row justify-content-center">
-<!--    <div class="col but-back position-absolute">-->
-<!--        <button class="btn btn-secondary m-5">späť na nákup</button>-->
-<!--    </div>-->
+<?php include "../components/variables.php";?>
 
-    <button class="btn btn-secondary circle-custom rounded-pill">1</button>
-    <button class="btn btn-secondary arrow-custom "><i class="fa fa-arrow-right m-0"></i></button>
-    <button class="btn btn-secondary circle-custom rounded-pill">2</button>
-    <button class="btn btn-secondary arrow-custom "><i class="fa fa-arrow-right m-0"></i></button>
-    <button class="btn btn-secondary circle-custom rounded-pill">3</button>
-    <button class="btn btn-secondary arrow-custom "><i class="fa fa-arrow-right m-0"></i></button>
-    <button class="btn btn-secondary circle-custom rounded-pill">4</button>
-    <button class="btn btn-secondary arrow-custom "><i class="fa fa-arrow-right m-0"></i></button>
-    <button class="btn btn-secondary circle-custom rounded-pill"><i class="fa-regular fa-circle-check"></i></button>
+<div class="row justify-content-center align-items-center">
+    <div class="d-none d-md-flex col-2 justify-content-center">
+        <a class="btn btn-secondary but-back"><i class="fa fa-arrow-left m-0"></i> na nákup</a>
+    </div>
 
+    <h1 class="d-flex d-sm-none text-light ps-5">Nákupný košík</h1>
+
+    <div class="col-12 col-md-8 d-none d-sm-flex flex-row justify-content-center align-items-center">
+
+        <div class="d-flex align-items-center flex-column circle-box-custom">
+            <button class="btn btn-secondary circle-custom rounded-pill">1</button>
+            <h6 class="text-light txt-custom">Košík</h6>
+        </div>
+        <button class="btn btn-secondary arrow-custom "><i class="fa fa-arrow-right m-0"></i></button>
+        <div class="d-flex align-items-center flex-column  circle-box-custom">
+            <button class="btn btn-secondary circle-custom rounded-pill disabled">2</button>
+            <h6 class="text-light txt-custom">Doprava</h6>
+        </div>
+        <button class="btn btn-secondary arrow-custom "><i class="fa fa-arrow-right m-0"></i></button>
+        <div class="d-flex align-items-center flex-column  circle-box-custom">
+            <button class="btn btn-secondary circle-custom rounded-pill disabled">3</button>
+            <h6 class="text-light txt-custom">Údaje</h6>
+        </div>
+        <button class="btn btn-secondary arrow-custom "><i class="fa fa-arrow-right m-0"></i></button>
+        <div class="d-flex align-items-center flex-column  circle-box-custom">
+            <button class="btn btn-secondary circle-custom rounded-pill disabled">4</button>
+            <h6 class="text-light txt-custom">Kontrola</h6>
+        </div>
+        <button class="btn btn-secondary arrow-custom"><i class="fa fa-arrow-right m-0"></i></button>
+        <div class="d-flex align-items-center flex-column  circle-box-custom">
+            <button class="btn btn-secondary circle-custom rounded-pill disabled"><i class="fa-regular fa-circle-check"></i></button>
+            <h6>hotovo</h6>
+        </div>
+
+    </div>
+
+    <div class="col-2"></div>
 </div>
 
 <div class="row justify-content-center">
 </div>
 
-<div class="row mt-5">
-    <div class="col-12 col-md-6 col-lg-4 mb-5 mb-lg-0 justify-content-center">
+<div class="row ms-lg-4">
+    <div class="col-12 col-md-6 col-lg-4 p-5 ps-md-5 pe-md-2 mb-5 p-lg-2 justify-content-center">
         <h4 class="text_color cart_text">Doprava</h4>
-        <div class="cart-slider justify-content-center mt-0 overflow-y-scroll overflow-x-hidden">
+        <div class="cart-slider justify-content-center mt-0 overflow-y-scroll overflow-x-hidden mx-custom">
             <div class="cart-product-small bg-light">
                 <div class="d-flex flex-row ms-3 w-100 align-items-center">
                     <i class="fa-solid fa-box-open fa-2x"></i>
@@ -91,9 +115,9 @@
         </div>
     </div>
 
-    <div class="col-12 col-md-6 col-lg-4 mb-5 mb-lg-0 justify-content-center">
+    <div class="col-12 col-md-6 col-lg-4 p-5 ps-md-2 pe-md-5 p-lg-2 justify-content-center">
         <h4 class="text_color cart_text">Spôsob platby</h4>
-        <div class="cart-slider justify-content-center mt-0 overflow-y-scroll overflow-x-hidden">
+        <div class="cart-slider justify-content-center mt-0 overflow-y-scroll overflow-x-hidden mx-custom">
             <div class="cart-product-small bg-light">
                 <div class="d-flex flex-row ms-3 w-100 align-items-center">
                     <i class="fa-regular fa-credit-card fa-2x"></i>
@@ -152,8 +176,37 @@
         </div>
     </div>
 
-    <div class="col-12 col-lg-4 mt-5 mt-lg-0 d-flex justify-content-center align-items-end">
-        <div class="total-custom">
+    <div class="col-12 col-lg-4 d-flex justify-content-center align-items-end">
+        <div class=" total-custom rounded-1 p-3 d-flex flex-column justify-content-between">
+            <div>
+                <h2 class="subtotal">Zhrnutie:</h2>
+                <div class="d-flex justify-content-between align-items-center m-3">
+                    <h5>Medzisúčet</h5>
+                    <h5>1200€</h5>
+                </div>
+                <div class="d-flex justify-content-between align-items-center m-3">
+                    <h5>Zľavy</h5>
+                    <h5>-200€</h5>
+                </div>
+                <div class="d-flex justify-content-between align-items-center m-3">
+                    <h5>Kupóny</h5>
+                    <h5>-40€</h5>
+                </div>
+            </div>
+
+            <div class="d-flex flex-column align-items-center">
+                <div class="d-flex justify-content-between align-items-center w-100 p-3 pb-0 pt-0">
+                    <h5>Počet položiek</h5>
+                    <h5>5ks</h5>
+                </div>
+                <hr class="line2-custom">
+                <div class="d-flex justify-content-between align-items-center w-100 m-3 p-3 pb-0 mt-1 pt-0">
+                    <h4>Súčet</h4>
+                    <h4>960€</h4>
+                </div>
+                <a href="../pages/cart_page3.php" class="btn mt-0 btn-dark mb-4">Pokračovať na vyplnenie údajov</a>
+            </div>
+
         </div>
     </div>
 </div>
