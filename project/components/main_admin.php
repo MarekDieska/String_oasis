332e75310c6b05a1909231e87e060f55da72b2ab
MarekDieska <xdieska@stuba.sk>
2025-03-29 23:19:07 +0100

    admin page update

--- project/components/main_admin.php
+++ project/components/main_admin.php
@@ -1,28 +1,29 @@
-<div class="mt-5 bg-dark text_color">
-    <div>
+<div class="card mt-5 bg-dark border-success-subtle text_color">
+    <div class="text-center m-3">
+        <div class="card-header text_colors">Admin - Pridanie produktu</div>
         <form id="productForm">
             <div class="row g-4">
                 <div class="col-md-8">
                     <div class="mb-2">
                         <label class="form-label fw-bold">Produkt</label>
-                        <input type="text" class="form-control" required maxlength="100" placeholder="Názov produktu">
+                        <input type="text" class="form-control text-black placeholder-black" maxlength="100" placeholder="Názov produktu">
                     </div>
                     <div class="mb-2">
                         <label class="form-label fw-bold">Popis produktu</label>
-                        <textarea class="form-control" rows="3" minlength="50" maxlength="500" placeholder="Detailný popis produktu"></textarea>
+                        <textarea class="form-control placeholder-black" rows="3" minlength="50" maxlength="500" placeholder="Detailný popis produktu"></textarea>
                     </div>
                     <div class="mb-2 parameters-section">
                         <label class="form-label fw-bold">Parametre</label>
                         <div class="parameter-fields">
                             <div class="row mb-3">
                                 <div class="col">
-                                    <input type="text" class="form-control" placeholder="Telo">
+                                    <input type="text" class="form-control placeholder-black" placeholder="Telo">
                                 </div>
                                 <div class="col">
-                                    <input type="text" class="form-control" placeholder="Krk">
+                                    <input type="text" class="form-control placeholder-black" placeholder="Krk">
                                 </div>
                                 <div class="col">
-                                    <input type="text" class="form-control" placeholder="Počet pražcov">
+                                    <input type="text" class="form-control placeholder-black" placeholder="Počet pražcov">
                                 </div>
                             </div>
                         </div>
@@ -34,10 +35,10 @@
                     <div class="mb-2">
                         <label class="form-label fw-bold">Cenové detaily</label>
                         <div class="input-group mb-3">
-                            <input type="number" class="form-control" placeholder="Cena">
+                            <input type="number" class="form-control placeholder-black" placeholder="Cena €">
                         </div>
                         <div class="input-group mb-3">
-                            <input type="number" class="form-control" placeholder="Zľava %">
+                            <input type="number" class="form-control placeholder-black" placeholder="Zľava %">
                         </div>
                     </div>
                     <div class="mb-2">
@@ -64,9 +65,9 @@
                 <div class="col-12">
                     <div class="mb-0">
                         <label class="form-label fw-bold mt-0">Obrázky</label>
-                        <div class="drop-zone p-5 text-center border rounded mb-1">
+                        <div class="drop-zone p-5 text-center border rounded mb-1 bg-dark">
                             <div class="drop-zone-thumb"></div>
-                            <p class="mb-0">Drag&Drop alebo vyhadať</p>
+                            <p class="mb-0 text-light">Drag&Drop alebo vyhľadať</p>
                         </div>
                     </div>
                 </div>
@@ -77,5 +78,47 @@
                 <button type="submit" class="btn btn-primary">Pridať produkt</button>
             </div>
         </form>
+    </div>
+</div>
+
+<hr class="text_color mt-5 mb-2">
+
+<div class="row w-100 justify-content-center">
+    <div class="col-12 col-lg-6 d-flex justify-content-center align-items-center p-5">
+        <div class="card bg-dark border-danger text-dark w-100">
+            <div class="card-header text-center text_color">Admin - Odstránenie produktu</div>
+            <div class="card-body">
+                <form id="productForm">
+                    <div class="mb-3">
+                        <input type="text" class="form-control text-black placeholder-black" maxlength="100" placeholder="Názov produktu">
+                    </div>
+                    <div class="mb-3">
+                        <select class="form-select">
+                            <option value="">Kategória</option>
+                            <option value="1">Gitara</option>
+                            <option value="2">Basgitara</option>
+                            <option value="3">Iné struny</option>
+                            <option value="4">Kombá</option>
+                            <option value="5">Príslušenstvo</option>
+                            <option value="6">Platne</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <select class="form-select">
+                            <option value="">Podkategória</option>
+                            <option value="1">Elektrické</option>
+                            <option value="2">Elektro-akustické</option>
+                            <option value="3">Akustické</option>
+                            <option value="4">Klasické</option>
+                            <option value="5">Detské</option>
+                        </select>
+                    </div>
+                    <div class="d-flex justify-content-end gap-2">
+                        <button type="button" class="btn btn-secondary">Zrušiť</button>
+                        <button type="submit" class="btn btn-danger">Odstrániť produkt</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
