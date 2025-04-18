b3fcccac1d6d5cae33e95d89caebc8884775b5a9
Viktória Latičová <xlaticova@stuba.sk>
2025-04-18 22:50:34 +0200

    fixnuty filter 2

--- project/resources/views/components/main_filter_r.blade.php
+++ project/resources/views/components/main_filter_r.blade.php
@@ -1,43 +1,77 @@
-<div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 prod-row">
-    @for ($i = 0; $i < count($nazvyProduktov); $i++)
-        <div class="col d-flex justify-content-center p-3 r-custom">
-            <div class="card products-card d-flex flex-column">
+<div class="container-fluid m-5 m-md-2 m-lg-4">
+    <div class="row row-cols-1 row-cols-lg-2 flex-row justify-content-between align-items-center">
+        <div class="col">
+            <h1 class="title-filter">Elektrické gitary</h1>
+        </div>
+        <div class="col d-flex flex-column flex-sm-row
+             justify-content-start justify-content-lg-end align-items-center">
+            <h5 class="pt-2">Zoradiť podľa:</h5>
+            <div class="btn-group sort-custom" role="group">
+                <input type="radio" class="btn-check" name="sortovanie" id="sort1" checked>
+                <label class="btn btn-outline-secondary" for="sort1">Názvu</label>
 
-                <div class="product-image">
-                    <img class="card-photo"
-                         src="{{ $obrazkyProduktov[$i % count($obrazkyProduktov)] }}"
-                         alt="{{ $nazvyProduktov[$i % count($nazvyProduktov)] }}">
-                </div>
+                <input type="radio" class="btn-check" name="sortovanie" id="sort2">
+                <label class="btn btn-outline-secondary" for="sort2">Ceny</label>
 
-                <div class="favorite-btn text-center">
-                    <p>&#x2661;</p>
-                </div>
+                <input type="radio" class="btn-check" name="sortovanie" id="sort3">
+                <label class="btn btn-outline-secondary" for="sort3">Hodnotenia</label>
+            </div>
 
-                <a class="cart-btn text-center" type="button" data-bs-toggle="modal" data-bs-target="#plusModal">
-                    <p>+</p>
-                </a>
+            <div class="btn-group sort2-custom" role="group">
+                <input type="radio" class="btn-check" name="zoradenie" id="sort11" checked>
+                <label class="btn btn-outline-secondary" for="sort11">
+                    <i class="fa fa-arrow-up-wide-short m-2"></i>
+                </label>
 
-                <a href="{{ route('produkt_detail') }}">
-                    <div class="text-custom">
-                        <h5 class="t1-custom">{{ $nazvyProduktov[$i % count($nazvyProduktov)] }}</h5>
-
-                        <div class="star-rating">
-                            @php
-                                $rating = $hodnoteniaProduktov[$i % count($hodnoteniaProduktov)];
-                            @endphp
-                            @for ($j = 1; $j <= 5; $j++)
-                                @if ($j <= $rating)
-                                    <i class="fa fa-star rating-color"></i>
-                                @else
-                                    <i class="fa fa-star rating-color2"></i>
-                                @endif
-                            @endfor
-                        </div>
-
-                        <h4 class="t2-custom">{{ $cenyProduktov[$i % count($cenyProduktov)] }}</h4>
-                    </div>
-                </a>
+                <input type="radio" class="btn-check" name="zoradenie" id="sort12">
+                <label class="btn btn-outline-secondary" for="sort12">
+                    <i class="fa fa-arrow-up-short-wide m-2"></i>
+                </label>
             </div>
         </div>
-    @endfor
+    </div>
+
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 prod-row">
+        @for ($i = 0; $i < count($nazvyProduktov); $i++)
+            <div class="col d-flex justify-content-center p-3 r-custom">
+                <div class="card products-card d-flex flex-column">
+
+                    <div class="product-image">
+                        <img class="card-photo"
+                             src="{{ $obrazkyProduktov[$i % count($obrazkyProduktov)] }}"
+                             alt="{{ $nazvyProduktov[$i % count($nazvyProduktov)] }}">
+                    </div>
+
+                    <div class="favorite-btn text-center">
+                        <p>&#x2661;</p>
+                    </div>
+
+                    <a class="cart-btn text-center" type="button" data-bs-toggle="modal" data-bs-target="#plusModal">
+                        <p>+</p>
+                    </a>
+
+                    <a href="{{ route('produkt_detail') }}">
+                        <div class="text-custom">
+                            <h5 class="t1-custom">{{ $nazvyProduktov[$i % count($nazvyProduktov)] }}</h5>
+
+                            <div class="star-rating">
+                                @php
+                                    $rating = $hodnoteniaProduktov[$i % count($hodnoteniaProduktov)];
+                                @endphp
+                                @for ($j = 1; $j <= 5; $j++)
+                                    @if ($j <= $rating)
+                                        <i class="fa fa-star rating-color"></i>
+                                    @else
+                                        <i class="fa fa-star rating-color2"></i>
+                                    @endif
+                                @endfor
+                            </div>
+
+                            <h4 class="t2-custom">{{ $cenyProduktov[$i % count($cenyProduktov)] }}</h4>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        @endfor
+    </div>
 </div>
